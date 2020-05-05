8d61ceab1bfec73ce1d232d0555e25e67b61251d
Diego ROJAS <diego.rojas@epitech.eu>
2020-05-05 11:05:13 +0200

    FIXED Norm on all 'enum' types.
    Changed all enum values to full uppercase, namely, 'tokent_e' and 'validator_checks_e'.

--- src/input/parser/token_validate_composite.c
+++ src/input/parser/token_validate_composite.c
@@ -35,7 +35,7 @@ const char *string, unsigned int *i, const char token)
 bool token_validate_composite(
 char const *string, unsigned int *i, const struct validator_s va)
 {
-    if (!(va.mask & vali_composite))
+    if (!(va.mask & VALI_COMPOSITE))
         return false;
     if (string[*i] == '\\') {
         if (string[*i + 1])
