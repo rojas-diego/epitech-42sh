f801caea0f73453ccf3bc0778348fc331f4c4256
Théo <theo.hinsinger@epitech.eu>
2020-05-06 06:42:23 +0200

    ADDED Builtin setenv's error handling

--- dev/builtins/src/setenv.c
+++ dev/builtins/src/setenv.c
@@ -16,19 +16,34 @@
 /* */
 #include "builtin/setenv.h"
 
-void builtin_setenv(const char * const *argv)
+static const char *SETENV_ERROR_MESSAGES[] = {
+    NULL,
+    "setenv: Too many arguments.\n",
+    "setenv: Variable name must contain alphanumeric characters.\n",
+    "setenv: Variable name must begin with a letter.\n",
+}
+
+enum builtin_setenv_e builtin_setenv(const char * const *argv)
 {
     if (!argv[0]) {
         builtin_display_env();
-        return;
+        return (SETENV_SUCCESS);
     }
     if (ptb_argv_length(argv) > 2) {
-        dprintf(2, "setenv: Too many arguments.\n");
-        return;
+        dprintf(2, SETENV_ERROR_MESSAGES[SETENV_TOO_MANY_ARGS]);
+        return (SETENV_TOO_MANY_ARGS);
+    }
+    if (!ptb_whitelist_alphanum(argv[0])) {
+        dprintf(2, SETENV_ERROR_MESSAGES[SETENV_ALPHANUM_ONLY]);
+        return (SETENV_ALPHANUM_ONLY);
+    }
+    if (!ptb_whitelist_digit(argv[0])) {
+        dprintf(2, SETENV_ERROR_MESSAGES[SETENV_MUST_BEGIN_WITH_A_LETTER]);
+        return (SETENV_MUST_BEGIN_WITH_A_LETTER);
     }
     if (setenv(argv[0], argv[1] ? argv[1] : "", 1) == -1) {
         dprintf(2, "ENOMEM\n");
-        return;
+        return (SETENV_ALLOCATION_FAIL);
     }
-    return;
+    return (SETENV_SUCCESS);
 }
