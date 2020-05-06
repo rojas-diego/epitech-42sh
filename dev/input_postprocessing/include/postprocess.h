612b6b07a636861f3db06437e1fcd588d25056cd
Théo <theo.hinsinger@epitech.eu>
2020-05-06 08:19:58 +0200

    ADDED Input_postprocressing lib
    Added Makefile for input_postprocessing lib
    Added includes
    Added 'quote_cleanup.c'

---
+++ dev/input_postprocessing/include/postprocess.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2019
+** PSU_42sh_2019
+** File description:
+** postprocess
+*/
+
+#ifndef POSTPROCESS_H_
+#define POSTPROCESS_H_
+
+#include "postprocess/quote_cleanup.h"
+
+#endif /* !POSTPROCESS_H_ */
