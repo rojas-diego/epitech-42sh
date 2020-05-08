f1b24c54b9ae65d75195737069643cb212c57824
Théo <theo.hinsinger@epitech.eu>
2020-05-08 19:04:12 +0200

    ADDED 'hexdumper.c'

---
+++ lib/parser_toolbox/include/parser_toolbox/hexdumper.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2019
+** PSU_42sh_2019
+** File description:
+** parser_toolbox hexdumper
+*/
+
+#ifndef PARSER_TOOLBOX_HEXDUMPER_H_
+#define PARSER_TOOLBOX_HEXDUMPER_H_
+
+#include <stdbool.h>
+
+void ptb_hexdumper(const void *mem, size_t size);
+
+#endif /* !PARSER_TOOLBOX_HEXDUMPER_H_ */
---
+++ lib/parser_toolbox/src/hexdumper.c
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_42sh_2019
+** File description:
+** hexdumper
+*/
+
+#include <stdio.h>
+#include <ctype.h>
+
+typedef unsigned long int size_t;
+
+static const char HEXA_STR[] = "0123456789abcdef";
+
+void ptb_hexdumper(const void *mem, size_t size)
+{
+    const char *str = (const char *) mem;
+    size_t pos = 0;
+
+    for (size_t sub_pos = 0; pos < size;) {
+        dprintf(1, "\033[38;2;150;150;220m%08zx\033[0m\t"
+            "\033[1m\033[38;2;180;123;90m", pos);
+        for (sub_pos = -1; ++sub_pos < 16 && pos < size; ++pos)
+            dprintf(1, "%c%c ", HEXA_STR[((unsigned char) str[pos]) / 16 % 16],
+                HEXA_STR[((unsigned char) str[pos]) % 16]);
+        pos -= sub_pos;
+        for (; sub_pos < 16; ++sub_pos)
+            dprintf(1, "00 ");
+        dprintf(1, "\033[0m\t\033[1m\033[38;2;187;115;85m|");
+        for (sub_pos = -1; ++sub_pos < 16 && pos < size; ++pos)
+            dprintf(1, "%c", isprint(str[pos]) ? str[pos] : '.');
+        for (; sub_pos < 16; ++sub_pos)
+            dprintf(1, " ");
+        dprintf(1, "|\033[0m\n");
+    }
+    dprintf(1, "\033[38;2;150;150;220m%08zx\033[0m\n", pos);
+}
