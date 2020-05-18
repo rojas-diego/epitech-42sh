e249cd0fe3b48370bbc7d1d0b6f4b70e2b1ac4cf
Emile Gurgand <egurgand@localhost.localdomain>
2020-05-18 14:25:01 +0200

    FIXED Compilation warning.
    The '#define' statement for '_GNU_SOURCE' wasn't located at the top of 'lib/input_postprocessing/src/env_var_replace.c' as per feature_test_macro(7).

---
+++ lib/input_postprocessing/include/gnu_source.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_42sh_2019
+** File description:
+** _GNU_SOURCE
+*/
+
+#ifndef PERSONNAL_DEF_GNU_SOURCE_H_
+#define PERSONNAL_DEF_GNU_SOURCE_H_
+
+#define _GNU_SOURCE
+#include <stdio.h>
+
+#endif /* !PERSONNAL_DEF_GNU_SOURCE_H_ */
