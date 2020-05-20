3ecc67d22431e30767b543ec4087fcd41d314977
Théo <theo.hinsinger@epitech.eu>
2020-05-20 14:56:57 +0200

    ADDED 'sort_string.c'

---
+++ lib/parser_toolbox/include/parser_toolbox/sort_string.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2020
+** parser_toolbox
+** File description:
+** parser_toolbox sort_string
+*/
+
+#ifndef PARSER_TOOLBOX_SORT_STRING_H_
+#define PARSER_TOOLBOX_SORT_STRING_H_
+
+#include <stddef.h>
+
+void ptb_sort_string(const char **strings, size_t length);
+
+#endif /* !PARSER_TOOLBOX_SORT_STRING_H_ */
