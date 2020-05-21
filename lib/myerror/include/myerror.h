ba36dd6b6db096e63286c8a65fed88995d3be581
Diego ROJAS <diego.rojas@epitech.eu>
2020-05-22 01:57:14 +0200

    ADDED myerror library.

---
+++ lib/myerror/include/myerror.h
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2019
+** myerror
+** File description:
+** myerror header
+*/
+
+#ifndef LIB_MY_ERROR_H_
+#define LIB_MY_ERROR_H_
+
+/* Structure Definitions */
+typedef enum {
+    err_write,
+    err_read
+} my_error_mode_t;
+
+/* Function Prototypes */
+int my_error(my_error_mode_t mode, int new_code);
+
+#endif
