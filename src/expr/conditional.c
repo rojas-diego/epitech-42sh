9ff807a669cf831cdbdb8129275c4b284ac66c13
Diego ROJAS <diego.rojas@epitech.eu>
2020-05-21 18:54:59 +0200

    ADDED 'conditional' rule behaviour.

--- src/expr/conditional.c
+++ src/expr/conditional.c
@@ -20,8 +20,27 @@ static struct expr_conditional_s *expr_conditional(
     struct grammar_s *this
 )
 {
-    (void)(this);
-    return NULL;
+    struct expr_conditional_s *exp = malloc(sizeof(struct expr_conditional_s));
+
+    if (!exp)
+        exit(84);
+    memset(exp, 0, sizeof(struct expr_conditional_s));
+    if (!grammar_match(this, 1, TOK_LPARANTH)) {
+        free(exp);
+        return NULL;
+    }
+    exp->lparanth = grammar_get_previous(this);
+    if (!grammar_match(this, 1, TOK_WORD)) {
+        free(exp);
+        return NULL;
+    }
+    exp->word = grammar_get_previous(this);
+    if (!grammar_match(this, 1, TOK_RPARANTH)) {
+        free(exp);
+        return NULL;
+    }
+    exp->rparanth = grammar_get_previous(this);
+    return exp;
 }
 
 struct expr_conditional_s *expr_conditional_w(
