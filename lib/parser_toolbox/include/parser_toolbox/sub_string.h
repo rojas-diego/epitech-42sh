1e9e7d9cead803f59e7ac7a0b8f7d8f687d20c93
Romain Minguet <romain.minguet@epitech.eu>
2020-05-21 04:34:20 -0400

    ADDED History replace function
    Added function that replaces all ! by matching previous commands.

---
+++ lib/parser_toolbox/include/parser_toolbox/sub_string.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2020
+** parser_toolbox
+** File description:
+** parser_toolbox sub_string
+*/
+
+#ifndef PARSER_TOOLBOX_SUB_STRING_H_
+#define PARSER_TOOLBOX_SUB_STRING_H_
+
+#include <stddef.h>
+
+char *ptb_sub_string(const char *str, size_t start, size_t end);
+
+#endif /* !PARSER_TOOLBOX_SUB_STRING_H_ */
