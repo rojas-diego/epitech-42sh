dd2fb8182f44767b15f64f417f61efddfc38c96f
Diego ROJAS <diego.rojas@epitech.eu>
2020-05-21 11:26:51 +0200

    FIXED 'control' expression 'statement' to NULL.

---
+++ src/expr/control.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2019
+** PSU_42sh_2019
+** File description:
+** expr_control
+*/
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "proto/grammar.h"
+#include "proto/expr.h"
+
+/*
+** @DESCRIPTION
+**   Rule for control expression.
+*/
+struct expr_control_s *expr_control(struct grammar_s *this)
+{
+    return NULL;
+}
