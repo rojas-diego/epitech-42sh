8c0766ed7d4a15c2c258d7b2d9b3def520fd530f
Théo <theo.hinsinger@epitech.eu>
2020-05-22 08:43:25 +0200

    ADDED Job 'create.c'

---
+++ include/proto/job/create.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_42sh_2019
+** File description:
+** create job
+*/
+
+#ifndef SH_PROTO_JOB_CREATE_H_
+#define SH_PROTO_JOB_CREATE_H_
+
+#include "types/job.h"
+
+struct job_s *job_create(int launch_id);
+
+#endif /* !SH_PROTO_JOB_CREATE_H_ */
