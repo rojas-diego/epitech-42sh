141291edd372ecdd9fe5e8ba0a897021f7d417a1
Théo <theo.hinsinger@epitech.eu>
2020-05-22 08:55:11 +0200

    ADDED Exec rule 'foreach.c'

---
+++ src/exec/rule/control/foreach.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_42sh_2019
+** File description:
+** exec rule control foreach
+*/
+
+#include "proto/exec/rule/control/foreach.h"
+
+int exec_rule_control_foreach(
+    struct sh *shell,
+    struct expr_foreach_control_s *rule
+)
+{
+    return (0);
+}
