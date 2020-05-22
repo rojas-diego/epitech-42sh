18e39ceee6704494ccf9551ec4c2d73b5e49e850
Théo <theo.hinsinger@epitech.eu>
2020-05-22 08:55:22 +0200

    ADDED Exec rule 'while.c'

---
+++ src/exec/rule/control/while.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_42sh_2019
+** File description:
+** exec rule control while
+*/
+
+#include "proto/exec/rule/control/while.h"
+
+int exec_rule_control_while(
+    struct sh *shell,
+    struct expr_while_control_s *rule
+)
+{
+    return (0);
+}
