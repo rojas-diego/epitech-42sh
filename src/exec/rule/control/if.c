ed59b21c0ceff77edc063d5f7ca9e4b2ccb7d0d9
Théo <theo.hinsinger@epitech.eu>
2020-05-22 08:55:13 +0200

    ADDED Exec rule 'if.c'

---
+++ src/exec/rule/control/if.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_42sh_2019
+** File description:
+** exec rule control if
+*/
+
+#include "proto/exec/rule/control/if.h"
+
+int exec_rule_control_if(
+    struct sh *shell,
+    struct expr_if_control_s *rule
+)
+{
+    return (0);
+}
