9b040abe342d2952a7f3a3162eb584673f48a082
Théo <theo.hinsinger@epitech.eu>
2020-05-22 08:53:09 +0200

    ADDED Exec rule 'conditional.c'

---
+++ src/exec/rule/conditional.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_42sh_2019
+** File description:
+** exec rule conditional
+*/
+
+#include "proto/exec/rule/conditional.h"
+
+int exec_rule_conditional(
+    struct sh *shell,
+    struct expr_conditional_s *rule
+)
+{
+    return (0);
+}
