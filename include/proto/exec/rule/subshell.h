a539517e1f94630e240cf284506a7f2743fcd9da
Théo <theo.hinsinger@epitech.eu>
2020-05-22 08:53:55 +0200

    ADDED Exec rule 'subshell.c'

---
+++ include/proto/exec/rule/subshell.h
@@ -0,0 +1,19 @@
+;/*
+** EPITECH PROJECT, 2020
+** PSU_42sh_2019
+** File description:
+** shell exec rule subshell
+*/
+
+#ifndef SH_SHELL_EXEC_RULE_SUBSHELL_H_
+#define SH_SHELL_EXEC_RULE_SUBSHELL_H_
+
+#include "types/shell.h"
+#include "types/expr.h"
+
+int exec_rule_subshell(
+    struct sh *shell,
+    struct expr_subshell_s *rule
+);
+
+#endif /* !SH_SHELL_EXEC_RULE_SUBSHELL_H_ */
---
+++ src/exec/rule/subshell.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_42sh_2019
+** File description:
+** exec rule subshell
+*/
+
+#include "proto/exec/rule/subshell.h"
+
+int exec_rule_subshell(
+    struct sh *shell,
+    struct expr_subshell_s *rule
+)
+{
+    return (0);
+}
