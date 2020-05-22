04da05824e69b13119995d7d58968d1d69eb32ee
Théo <theo.hinsinger@epitech.eu>
2020-05-22 08:53:59 +0200

    ADDED Exec rule 'wordlist.c'

---
+++ src/exec/rule/wordlist.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_42sh_2019
+** File description:
+** exec rule wordlist
+*/
+
+#include "proto/exec/rule/wordlist.h"
+
+int exec_rule_wordlist(
+    struct sh *shell,
+    struct expr_wordlist_s *rule
+)
+{
+    return (0);
+}
