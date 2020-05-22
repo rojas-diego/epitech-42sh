1a78ba79fa4d615eff4753a484920d820637b321
Théo <theo.hinsinger@epitech.eu>
2020-05-22 08:55:08 +0200

    ADDED Exec rule 'else_if.c'

---
+++ include/proto/exec/rule/control/else_if.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_42sh_2019
+** File description:
+** shell exec rule control else_if
+*/
+
+#ifndef SH_SHELL_EXEC_RULE_CONTROL_ELSE_IF_H_
+#define SH_SHELL_EXEC_RULE_CONTROL_ELSE_IF_H_
+
+#include "types/shell.h"
+#include "types/expr.h"
+
+
+#endif /* !SH_SHELL_EXEC_RULE_CONTROL_ELSE_IF_H_ */
---
+++ src/exec/rule/control/else_if.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_42sh_2019
+** File description:
+** exec rule control else_if
+*/
+
+#include "proto/exec/rule/control/else_if.h"
+
+int exec_rule_control_else_if(
+    struct sh *shell,
+    struct expr_else_if_control_s *rule
+)
+{
+    return (0);
+}
