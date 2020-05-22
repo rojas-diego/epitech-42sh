11c6ea67993c9991604188ba2530b95dad65093f
Théo <theo.hinsinger@epitech.eu>
2020-05-22 08:55:17 +0200

    ADDED Exec rule 'repeat.c'

---
+++ include/proto/exec/rule/control/repeat.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_42sh_2019
+** File description:
+** shell exec rule control repear
+*/
+
+#ifndef SH_SHELL_EXEC_RULE_CONTROL_REPEAT_H_
+#define SH_SHELL_EXEC_RULE_CONTROL_REPEAT_H_
+
+#include "types/shell.h"
+#include "types/expr.h"
+
+
+#endif /* !SH_SHELL_EXEC_RULE_CONTROL_REPEAT_H_ */
---
+++ src/exec/rule/control/repeat.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_42sh_2019
+** File description:
+** exec rule control repeat
+*/
+
+#include "proto/exec/rule/control/repeat.h"
+
+int exec_rule_control_repeat(
+    struct sh *shell,
+    struct expr_repeat_control_s *rule
+)
+{
+    return (0);
+}
