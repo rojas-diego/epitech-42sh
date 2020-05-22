71a1493ce7f73e42e0ff3bc1a25529d9163e75db
Théo <theo.hinsinger@epitech.eu>
2020-05-22 08:53:13 +0200

    ADDED Exec rule 'control.c'

---
+++ src/exec/rule/control.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_42sh_2019
+** File description:
+** exec rule control
+*/
+
+#include "proto/exec/rule/control.h"
+
+int exec_rule_control(
+    struct sh *shell,
+    struct expr_control_s *rule
+)
+{
+    return (0);
+}
