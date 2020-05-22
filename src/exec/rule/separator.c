3a1c56462e9a15380551e748bec6b131c7539579
Théo <theo.hinsinger@epitech.eu>
2020-05-22 08:53:45 +0200

    ADDED Exec rule 'separator.c'

---
+++ src/exec/rule/separator.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_42sh_2019
+** File description:
+** exec rule separator
+*/
+
+#include "proto/exec/rule/separator.h"
+
+int exec_rule_separator(
+    struct sh *shell,
+    struct expr_separator_s *rule
+)
+{
+    return (0);
+}
