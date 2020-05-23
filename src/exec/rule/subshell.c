7e1d8a82f9b01476130ea3be9655f08d3df8543d
Théo <theo.hinsinger@epitech.eu>
2020-05-23 23:13:29 +0200

    ADDED Subshell handling

--- src/exec/rule/subshell.c
+++ src/exec/rule/subshell.c
@@ -7,15 +7,28 @@
 
 #include "proto/exec/rule/debug.h"
 
+#include "proto/exec/rule/block.h"
 #include "proto/exec/rule/subshell.h"
+#include "proto/token/get_string.h"
+#include "proto/job/process/create.h"
+#include "proto/job/process/append.h"
+
+#include "proto/exec/magic/parse.h"
 
 int exec_rule_subshell(
-    __attribute__((unused)) struct sh *shell,
-    __attribute__((unused)) struct expr_subshell_s *rule,
-    __attribute__((unused)) struct job_s *job
+    struct sh *shell,
+    struct expr_subshell_s *rule,
+    struct job_s *job
 )
 {
+    struct process_s *process = process_create();
+    unsigned int save = rule->lparanth->end;
+
     exec_rule_debug(shell, "subshell", true);
+    rule->lparanth->end = rule->rparanth->end;
+    token_get_string(rule->lparanth, shell->rawinput);
+    rule->lparanth->end = save;
+    job_process_append(job, process);
     exec_rule_debug(shell, "subshell", false);
     return (0);
 }
