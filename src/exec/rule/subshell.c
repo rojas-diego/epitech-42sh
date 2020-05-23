4bdd2f435c67d4f312366379ff05a6044fb77b4a
Théo <theo.hinsinger@epitech.eu>
2020-05-23 22:21:17 +0200

    FIXED Compilation

--- src/exec/rule/subshell.c
+++ src/exec/rule/subshell.c
@@ -5,38 +5,17 @@
 ** exec rule subshell
 */
 
-#include <unistd.h>
-#include <stdlib.h>
-
 #include "proto/exec/rule/debug.h"
 
 #include "proto/exec/rule/subshell.h"
 
-static void exec_do_subshell(
-    struct sh *shell,
-    struct expr_subshell_s *rule
-)
-{
-    pid_t pid;
-    int fd[2];
-
-    if (pipe(fd) == -1) {
-    }
-    pid = fork();
-    if (pid < 0) {
-        exit(1);
-    } else if (pid == 0) {
-        exit(1);
-    }
-}
-
 int exec_rule_subshell(
     struct sh *shell,
-    struct expr_subshell_s *rule
+    struct expr_subshell_s *rule,
+    struct job_s *job
 )
 {
     exec_rule_debug(shell, "subshell", true);
-    exec_do_subshell(shell, rule);
     exec_rule_debug(shell, "subshell", false);
     return (0);
 }
