a7cb18877464031d283fed5f561fef32bb04ffc1
Théo <theo.hinsinger@epitech.eu>
2020-05-23 13:57:50 +0200

    FIXED Warnings implicit declaration

---
+++ include/proto/token/get_string.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2019
+** PSU_42sh_2019
+** File description:
+** token header file.
+*/
+
+#ifndef SH_PROTO_INPUT_TOKEN_GET_STRING_H_
+#define SH_PROTO_INPUT_TOKEN_GET_STRING_H_
+
+#include "types/token.h"
+
+/* Belongs to src/input/parser/token.c */
+char *token_get_string(const struct token_s *this, const char *rawinput);
+
+#endif
