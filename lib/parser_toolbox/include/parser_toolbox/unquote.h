d69a47caa0e7c441886e350685ab65736130e4ac
Théo <theo.hinsinger@epitech.eu>
2020-05-23 21:25:21 +0200

    ADDED 'unquote.c'

---
+++ lib/parser_toolbox/include/parser_toolbox/unquote.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2020
+** parser_toolbox
+** File description:
+** parser_toolbox unquote
+*/
+
+#ifndef PARSER_TOOLBOX_UNQUOTE_H_
+#define PARSER_TOOLBOX_UNQUOTE_H_
+
+void ptb_unquote(char *str);
+
+#endif /* !PARSER_TOOLBOX_UNQUOTE_H_ */
