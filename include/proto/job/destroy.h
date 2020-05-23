38c91fd24d170d3c32c7473102052b4b7ce501c2
Théo <theo.hinsinger@epitech.eu>
2020-05-23 09:30:28 +0200

    ADDED Job Destroy

---
+++ include/proto/job/destroy.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_42sh_2019
+** File description:
+** destroy job
+*/
+
+#ifndef SH_PROTO_JOB_DESTROY_H_
+#define SH_PROTO_JOB_DESTROY_H_
+
+#include "types/job.h"
+
+void job_destroy(struct job_s *job);
+
+#endif /* !SH_PROTO_JOB_DESTROY_H_ */
