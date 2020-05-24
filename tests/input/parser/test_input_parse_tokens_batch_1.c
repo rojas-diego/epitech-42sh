b7f063fcbfd1a1edf521be3ca9745f34757ab50e
Diego ROJAS <diego.rojas@epitech.eu>
2020-05-24 11:21:55 +0200

    TESTED Token batch.

---
+++ tests/input/parser/test_input_parse_tokens_batch_1.c
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_42sh_2019
+** File description:
+** input_parse_tokens - test
+*/
+
+#include <criterion/criterion.h>
+#include <string.h>
+
+#include "tests/mock_types.h"
+#include "proto/input/parser.h"
+
+Test(input_parse_tokens, batch_1)
+{
+    struct sh shell = MOCK_SH;
+    struct node_s *curr;
+    struct token_s *token;
+    enum tokent_e types[] = {TOK_WORD, TOK_GREAT, TOK_DGREAT, TOK_LESS,
+    TOK_DLESS, TOK_PIPE, TOK_OR_IF, TOK_ENDIF, TOK_WHILE, TOK_IF,
+    TOK_GREAT, TOK_WORD, TOK_EOF};
+
+    shell.rawinput = strdup("word> >> < << | || endif while if> whil");
+    input_parse_tokens(&shell);
+    curr = shell.tokens;
+    for (unsigned int i = 0; types[i] != TOK_EOF; i++) {
+        if (!curr) {
+            cr_assert(0);
+            return;
+        }
+        token = curr->data;
+        cr_assert_eq(types[i], token->type);
+        curr = curr->next;
+    }
+}
