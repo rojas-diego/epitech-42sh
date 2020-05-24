b087ae2c9ddb76ee3fa383999ddf20f2e6f6bb41
Diego ROJAS <diego.rojas@epitech.eu>
2020-05-24 21:55:04 +0200

    ADDED Tests for simple command grammars.

--- tests/grammar/test_grammar.c
+++ tests/grammar/test_grammar.c
@@ -17,19 +17,27 @@
 Test(input_parse_grammar, simple_command)
 {
     struct sh shell = MOCK_SH;
+    struct expr_pipeline_s *expr = NULL;
 
     shell.rawinput = strdup("ls");
     input_parse_tokens(&shell);
     input_parse_grammar(&shell);
+    expr = shell.expression->block->statement->compound->grouping->pipeline;
+    cr_assert_eq(expr->command->word->type, TOK_WORD);
+    cr_assert_eq(expr->command->word->start, 0);
+    cr_assert_eq(expr->command->word->end, 2);
 }
 
 Test(input_parse_grammar, advanced_command)
 {
     struct sh shell = MOCK_SH;
+    struct expr_pipeline_s *expr = NULL;
 
     shell.rawinput = strdup("ls | cat -e || ls \"this\" && ls > file");
     input_parse_tokens(&shell);
     input_parse_grammar(&shell);
+    expr = shell.expression->block->statement->compound->grouping->pipeline;
+    cr_assert_eq(expr->pipe->type, TOK_PIPE);
 }
 
 Test(input_parse_grammar, control_flow)
@@ -43,4 +51,15 @@ Test(input_parse_grammar, control_flow)
     expression = shell.expression->block->statement->control->if_control;
     cr_assert_not_null(shell.expression);
     cr_assert_not_null(expression);
+}
+
+Test(input_parse_grammar, bad_control_flow)
+{
+    struct sh shell = MOCK_SH;
+    struct expr_if_control_s *expression = NULL;
+
+    shell.rawinput = strdup("if then\nls\nelse if (0) then\nls\nendif\n");
+    input_parse_tokens(&shell);
+    input_parse_grammar(&shell);
+    cr_assert_null(expression);
 }
